173f658819db6900e10fcec322191e83f23c83e7
leechigu <78770548+leechigu@users.noreply.github.com>
2022-01-15 14:59:41 +0900

    Initial commit

---
+++ src/baekjoon15649.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class baekjoon15649 {
+
+    static ArrayList<ArrayList<Integer>> result;
+
+    static int cnt = 0;
+
+    public void numset(int n,boolean[] isChecked ,int m){
+
+        for(int i = 0;i<isChecked.length;i++){
+            isChecked[i] =false;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int n;
+        int m;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+
+
+    }
+}
