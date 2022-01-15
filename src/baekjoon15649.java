36d0b300e66d3fe205296584b4f8b7ee838d2e5c
leechigu <78770548+leechigu@users.noreply.github.com>
2022-01-15 15:28:03 +0900

    자바 permutation 연습 백준 15649번 dfs-백트래킹 활용

--- src/baekjoon15649.java
+++ src/baekjoon15649.java
@@ -1,32 +1,49 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class baekjoon15649 {
-
-    static ArrayList<ArrayList<Integer>> result;
-
-    static int cnt = 0;
-
-    public void numset(int n,boolean[] isChecked ,int m){
-
-        for(int i = 0;i<isChecked.length;i++){
-            isChecked[i] =false;
+    static int n;
+    static int m;
+    static int[] resultArray;
+    static boolean[] isVisited;
+    static int[] initArray;
+    public static void permutation(int depth){
+        if(depth ==m){
+            print();
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(!isVisited[i]){
+                isVisited[i] =true;
+                resultArray[depth] =initArray[i];
+                permutation(depth+1);
+                isVisited[i] =false;
+            }
         }
 
-
     }
 
+    static void print(){
+        for(int i=0;i<m;i++){
+            System.out.print(resultArray[i] + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-
-        int n;
-        int m;
-        Scanner sc = new Scanner(System.in);
-
+        Scanner sc  = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-
-
+        initArray = new int[n];
+        for(int i = 0;i< initArray.length;i++){
+            initArray[i] =i+1;
+        }
+        resultArray =new int[n];
+        isVisited = new boolean[n];
+        for(int i =0; i< isVisited.length;i++){
+            isVisited[i] =false;
+        }
+        permutation(0);
 
     }
+
 }
