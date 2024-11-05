b844ae4a33ba6b3b338a75ae75a506c9ee988d3a
leechigu279 <ttee2347@nate.com>
2024-11-05 17:00:44 +0900

    영차영차 어우..

---
+++ src/Lcs.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lcs {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+
+
+
+    }
+}
