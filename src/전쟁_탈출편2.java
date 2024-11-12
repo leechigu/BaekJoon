df1730bcd309877c1e47aca3562cf8935997f911
leechigu279 <ttee2347@nate.com>
2024-11-12 11:10:05 +0900

    영차영차

--- src/전쟁_탈출편2.java
+++ src/전쟁_탈출편2.java
@@ -10,8 +10,11 @@ public class 전쟁_탈출편2 {
     static int[] min;
     static int[] min2;
     static boolean[] isVisited;
+    static int[] prevNum;
     static ArrayList<Integer> lastList;
+
     static void bfs(){
+
         PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -24,6 +27,8 @@ public class 전쟁_탈출편2 {
             int curIndx = cur[0];
             int curVal = cur[1];
 
+            prevNum[1] = -1;
+
             if(isVisited[curIndx])
                 continue;
 
@@ -34,6 +39,7 @@ public class 전쟁_탈출편2 {
                 int nextVal = next[1]+curVal;
 
                 if(min[nextIndx]>nextVal) {
+                    prevNum[nextIndx] = curIndx;
                     min[nextIndx] = nextVal;
                     queue.add(new int[]{nextIndx, nextVal});
                 }
@@ -54,9 +60,13 @@ public class 전쟁_탈출편2 {
             lists[i] = new ArrayList<>();
         lastList = new ArrayList<>();
         isVisited = new boolean[n+1];
+        prevNum = new int[n+1];
         min = new int[n+1];
         min2 = new int[n+1];
         Arrays.fill(min, Integer.MAX_VALUE);
+        min[1] = 0;
+        Arrays.fill(prevNum, -1);
+        //Arrays.fill(min2, Integer.MAX_VALUE);
 
 
         for(int i=1;i<=m;i++){
@@ -67,9 +77,46 @@ public class 전쟁_탈출편2 {
             int c = Integer.parseInt(st.nextToken());
 
             lists[a].add(new int[]{b,c});
+            lists[b].add(new int[]{a,c});
         }
 
         bfs();
+
+        //lists에서 최단거리 간선 제거
+        int curIndx = n;
+        while(true){
+            if(prevNum[curIndx]==-1)
+                break;
+
+            int prev = prevNum[curIndx];
+
+
+            int val = min[curIndx] - min[prev];
+            //System.out.println(prev +" - >"+ curIndx + " val  = " + val);
+            //System.out.println(prev +" lists size + " + lists[prev].size());
+            //System.out.println(curIndx +" lists size + " + lists[curIndx].size());
+            for (int i = 0; i < lists[prev].size(); i++) {
+                int[] x = lists[prev].get(i);
+                if (x[0] == curIndx && x[1] == val) {
+                    int[] remove = lists[prev].remove(i);
+                    //System.out.println(prev + " " + remove[0] + " " + remove[1]);
+                    break;
+                }
+            }
+            for (int i = 0; i < lists[curIndx].size(); i++) {
+                int[] x = lists[curIndx].get(i);
+                if (x[0] == prev && x[1] == val) {
+                    int[] remove = lists[curIndx].remove(i);
+                    //System.out.println(remove[0] + " " + curIndx + " " + remove[1]);
+                    break;
+                }
+            }
+            curIndx = prev;
+        }
+
+        Arrays.fill(min, Integer.MAX_VALUE);
         isVisited = new boolean[n+1];
+        bfs();
+        System.out.println(min[n]);
     }
 }
