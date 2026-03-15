2234516ef6abc72f4fd7162c7f8e4b0db71be716
rrww2 <ttee2347@nate.com>
2026-03-15 18:21:57 +0900

    시간지나면 까먹어버리네 ㅠㅠ;

--- src/행성터널.java
+++ src/행성터널.java
@@ -5,112 +5,102 @@ import java.util.*;
 
 public class 행성터널 {
 
-    static List<int[]> lists = new ArrayList<>();
     static int n;
-    static int[] parents;
-    static int[] childCnt;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-
-        int[][] arrX = new int[n][2];
-        int[][] arrY = new int[n][2];
-        int[][] arrZ = new int[n][2];
-
-        for(int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            arrX[i][0] = i;
-            arrX[i][1] = Integer.parseInt(st.nextToken());
-            arrY[i][0] = i;
-            arrY[i][1] = Integer.parseInt(st.nextToken());
-            arrZ[i][0] = i;
-            arrZ[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arrX, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1]-o2[1];
-            }
-        });
-        Arrays.sort(arrY, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1]-o2[1];
-            }
-        });
-        Arrays.sort(arrZ, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1]-o2[1];
-            }
-        });
-
-        setLists(arrX);
-        setLists(arrY);
-        setLists(arrZ);
-
-        parents = new int[n];
-        childCnt = new int[n];
-        for(int i=0;i<n;i++){
-            parents[i] = i;
-            childCnt[i] = 1;
-        }
-
-
-        lists.sort(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[2] - o2[2];
-            }
-        });
-
-        long answer = 0;
-        for (int[] info : lists) {
-
-            int a = info[0];
-            int b = info[1];
-            int val = info[2];
-
-            if (findRoot(a) != findRoot(b)) {
-                union(a, b);
-                answer += val;
-            }
-        }
-
-        System.out.print(answer);
-
-    }
-
-    static int findRoot(int a){
-        if(parents[a] == a)
-            return a;
-        return parents[a] = findRoot(parents[a]);
-    }
+    static int[] parent;
 
     static void union(int a,int b){
         int rootA = findRoot(a);
         int rootB = findRoot(b);
 
-        if(childCnt[rootA]>childCnt[rootB]){
-            parents[rootB] = rootA;
-            childCnt[rootA] += childCnt[rootB];
-        }else{
-            parents[rootA] = rootB;
-            childCnt[rootB] += childCnt[rootA];
-        }
+        int minRoot = Math.min(rootA,rootB);
+        int maxRoot = Math.max(rootA,rootB);
+
+        parent[maxRoot] = minRoot;
     }
 
-    static void setLists(int[][] arr){
-        for(int i=0;i<n-1;i++){
-            int a = arr[i][0];
-            int b = arr[i+1][0];
-            int val = Math.abs(arr[i][1] - arr[i+1][1]);
-            lists.add(new int[]{a,b,val});
-        }
+    static int findRoot(int a){
+        if(a==parent[a])
+            return a;
+        return parent[a] = findRoot(parent[a]);
     }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        parent = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+
+        int[][] map = new int[n][4];
+
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            int indx = i;
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+
+            map[i][0] = indx;
+            map[i][1] = x;
+            map[i][2] = y;
+            map[i][3] = z;
+        }
+        Queue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+        //x를 기준으로 sort한다.
+        Arrays.sort(map, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
+            }
+        });
+
+        for(int i=1;i<n;i++)
+            queue.add(new int[]{map[i-1][0],map[i][0],Math.abs(map[i][1]-map[i-1][1])});
+
+        //y를 기준으로 sort한다.
+        Arrays.sort(map, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2]-o2[2];
+            }
+        });
+
+        for(int i=1;i<n;i++)
+            queue.add(new int[]{map[i-1][0],map[i][0],Math.abs(map[i][2]-map[i-1][2])});
+
+        //z를 기준으로 sort한다.
+        Arrays.sort(map, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[3]-o2[3];
+            }
+        });
+
+        for(int i=1;i<n;i++)
+            queue.add(new int[]{map[i-1][0],map[i][0],Math.abs(map[i][3]-map[i-1][3])});
+
+        long answer = 0;
+        while(!queue.isEmpty()){
+
+            int[] cur = queue.poll();
+
+            int a = cur[0];
+            int b = cur[1];
+            int val = cur[2];
+
+            if(findRoot(a)!=findRoot(b)){
+                union(a,b);
+                answer+=val;
+            }
+        }
+        System.out.print(answer);
+    }
 }
